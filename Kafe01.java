9320e143a9d2423488dfab52737875a5caf8f90f
Abimmmm <156158397+Abimmmm@users.noreply.github.com>
2024-11-22 13:33:32 +0700

    Percobaan 1

---
+++ Kafe01.java
@@ -0,0 +1,17 @@
+public class Kafe01 {
+
+    public static void main(String[] args) {
+        Menu();
+    }
+    public static void Menu(){
+        System.out.println("===== MENU RESTO KAFE =====");
+        System.out.println("1. Kopi Hitam - Rp 15,000");
+        System.out.println("2. Cappuccino - Rp 20,000");
+        System.out.println("1. Latte - Rp 22,000");
+        System.out.println("1. Teh Tarik - Rp 12,000");
+        System.out.println("1. Roti Bakar - Rp 10,000");
+        System.out.println("1. Mie Goreng - Rp 18,000");
+        System.out.println("============================");
+        System.out.println("Silahkan pilih menu yang anda inginkan.");
+    }
+}
